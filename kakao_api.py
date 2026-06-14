9999dfb34f3e83e1c98e125e3b7bd4f3818e483a
hanyusok <hanyusok@gmail.com>
2026-06-14 17:38:14 +0900

    fix: add missing closing quotation mark to KAKAO_REST_API_KEY string

--- kakao_api.py
+++ kakao_api.py
@@ -1,7 +1,7 @@
 import requests
 
 # Kakao Local API requires the REST API Key. Using the JavaScript Key here will cause a 401 Client Error.
-KAKAO_REST_API_KEY = "80f26a55a496db9a8320511b82d02f63
+KAKAO_REST_API_KEY = "80f26a55a496db9a8320511b82d02f63"
 
 def geocode_address(address: str):
     """
